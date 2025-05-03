4848f354720a6ddd2ff92ede26f9da49ac4269af
fordecember <1921567337@qq.com>
2025-05-03 17:44:21 +0800

    chore: 添加新依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,55 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+import {VantResolver} from "@vant/auto-import-resolver";
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+import ElementPlus from "unplugin-element-plus/vite";
+
+import path from 'path';
+
+
+const pathSrc = path.resolve(__dirname, "src");
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or 'modern'
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    tailwindcss(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'useMessage',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ],
+      resolvers: [ElementPlusResolver(), NaiveUiResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver(), NaiveUiResolver(),VantResolver()],
+    }),
+    ElementPlus({}),
+  ],
+  resolve: {
+    alias: {
+      "@": pathSrc,
+    },
+  }
 })
